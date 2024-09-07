d3d4ad6a58a8cc0bd031077b49c2afa24f721372
mandel84 <mandel84@github.com>
2024-09-07 19:06:35 +0300

    test

--- task-manager/client/src/store/index.js
+++ task-manager/client/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import taskReducer from '../reducers/taskReducer';
+import {taskReducer} from '../reducers/taskReducer';
 import projectReducer from '../reducers/projectReducer';
 
 
